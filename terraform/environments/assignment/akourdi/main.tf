4a02446aa7b00ed993c364b779a1f4073a9676cc
AmirKourdi <amir_kourdi@hotmail.com>
2021-12-28 01:23:45 +0200

    Update main.tf

--- terraform/environments/assignment/akourdi/main.tf
+++ terraform/environments/assignment/akourdi/main.tf
@@ -6,16 +6,7 @@ provider "aws" {
   region = "eu-west-1"
 }
 
-locals {
-  ami_owner          = "721645178643"
-  admin_ami          = "assignment-v9-admin-final"
-  candidate_name     = "assignment"
-  assignment_version = "v9"
-  env                = "stage-v9"
-}
 
 module "assignment_deployment" {
   source = "../../../modules/aws-assignment-deployment"
-
-
 }
